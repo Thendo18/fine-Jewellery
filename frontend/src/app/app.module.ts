1e80a534390ec35368dcc55935d5e2cc0191d0cf
Thendo Makherana <makherana@thedigitalacademy.co.za>
2022-08-03 12:11:47 +0200

    installing and structuring the project

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,40 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './pages/login/login.component';
+import { RegisterComponent } from './pages/register/register.component';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
+import { FooterComponent } from './components/footer/footer.component';
+import { NotfoundComponent } from './components/notfound/notfound.component';
+import { ProductViewComponent } from './components/product-view/product-view.component';
+import { OrdersComponent } from './components/orders/orders.component';
+import { ProductsComponent } from './components/products/products.component';
+import { ShoppingCartComponent } from './components/shopping-cart/shopping-cart.component';
+import { UserprofileComponent } from './components/userprofile/userprofile.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    LandingPageComponent,
+    NavBarComponent,
+    FooterComponent,
+    NotfoundComponent,
+    ProductViewComponent,
+    OrdersComponent,
+    ProductsComponent,
+    ShoppingCartComponent,
+    UserprofileComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
