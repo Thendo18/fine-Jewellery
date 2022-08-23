dd67f4bca3ee5f2f0e7e61d1bd567d7e4ab7c403
Thendo Makherana <makherana@thedigitalacademy.co.za>
2022-08-23 10:43:55 +0200

    changes

--- frontend/src/app/components/userprofile/userprofile.component.ts
+++ frontend/src/app/components/userprofile/userprofile.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { AuthenticationService } from 'src/app/services/authentication.service';
 
 @Component({
   selector: 'app-userprofile',
@@ -6,10 +8,30 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./userprofile.component.scss']
 })
 export class UserprofileComponent implements OnInit {
-
-  constructor() { }
+  currentuser: Object = {}
+  userProfile: any 
+  profile:any
+  name: any 
+  email: any
+  constructor(
+    private actRoute: ActivatedRoute,
+    private auth:AuthenticationService) {
+      // let id = this.actRoute.snapshot.paramMap.get('id');
+      // console.log(id);
+     }
 
   ngOnInit(): void {
+    // this.user()
   }
-
+ 
+  // user(){
+  //   return this.auth.getUserProfile().subscribe({next:data => {
+  //     this.profile = data
+  //     this.name = this.profile.decoded.name
+  //     this.email = this.profile.decoded.email
+  //   }})
+  // }
 }
+
+
+
